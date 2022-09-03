453e21252e8086b0dc625c034751a7e0d65076f8
saya <saya@saya-mac-book.local>
2022-09-03 12:58:37 +0900

    updated redux middleware

--- redux/Store.tsx
+++ redux/Store.tsx
@@ -2,6 +2,14 @@ import {configureStore} from '@reduxjs/toolkit';
 import rootReducer from './reducers/Index';
 
 export default function configureStoreWrap() {
-  const store = configureStore({reducer: rootReducer});
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+        immutableCheck: {warnAfter: 300},
+        serializableCheck: {warnAfter: 300},
+      }),
+  });
+
   return store;
 }
